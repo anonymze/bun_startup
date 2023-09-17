f9dc526e4e36a458337249346632743d9d8a4eeb
yann <ymetier@openstudio.fr>
2023-09-17 22:55:55 +0200

    add: event actions user update

--- server/discord/commands/interaction_commands.ts
+++ server/discord/commands/interaction_commands.ts
@@ -1,9 +1,10 @@
+import { Events } from "discord.js";
 import { instanceClientDiscord } from "../bot";
 
 /** COMMANDS REGISTERED WILL BE INTERACTED HERE **/
 
 // interac with commands
-instanceClientDiscord.on('interactionCreate', async interaction => {
+instanceClientDiscord.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
   
     if (interaction.commandName === 'ping_pong') {
@@ -13,4 +14,11 @@ instanceClientDiscord.on('interactionCreate', async interaction => {
 
     await interaction.reply('Bot is running! BUT NO COMMANDS FOUND');
     return;
+});
+
+// interac with actions
+instanceClientDiscord.on(Events.UserUpdate, (oldUser, newUser) => {
+  if (oldUser.avatarURL === newUser.avatarURL) return;
+
+  
 });
