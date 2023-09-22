e987776f50c30b418cb69032ffdd3559ff95d55f
anonymze <anodevfr@gmail.com>
2023-09-22 21:29:10 +0200

    add: comment on relative path config

--- server/db/drizzle.config.ts
+++ server/db/drizzle.config.ts
@@ -4,6 +4,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
  export default {
+  // cant make relative paths because the script start froml package.json i guess...
   schema: "server/db/schema/**/schema.ts",
   out: "server/db/migrations",
   driver: 'pg',
--- server/db/drizzle.migrate.ts
+++ server/db/drizzle.migrate.ts
@@ -1,5 +1,6 @@
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import { dbInstance} from "./drizzle.config";
  
+// cant make relative paths because the script start froml package.json i guess...
 await migrate(dbInstance, { migrationsFolder: "server/db/migrations" });
 
