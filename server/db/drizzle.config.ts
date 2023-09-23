0078c99aaecb53aad71ced15bcb673b708e673b9
anonymze <anodevfr@gmail.com>
2023-09-23 09:27:07 +0200

    fix: db credentials

--- server/db/drizzle.config.ts
+++ server/db/drizzle.config.ts
@@ -3,14 +3,16 @@ import { ENV } from "env";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
+console.log(ENV.DATABASE_STRING);
+
 export default {
   // can't make relative paths because the script start from package.json i guess...
   schema: "server/db/schema/**/schema.ts",
   out: "server/db/migrations",
   driver: "pg",
-  dbCredentials: {
-    connectionString: ENV.DATABASE_STRING,
-  },
+  // dbCredentials: {
+  //   connectionString: ENV.DATABASE_STRING,
+  // },
 } satisfies Config;
 
-export const dbInstance = drizzle(postgres({ max: 1 }));
+export const dbInstance = drizzle(postgres(ENV.DATABASE_STRING, { max: 1 }));
